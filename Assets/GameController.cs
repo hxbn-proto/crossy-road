4fceae4558df12fd6826afd691b1a84c6c06a71c
hxbn-proto <chernik.lut@gmail.com>
2021-11-16 20:19:51 +0100

    Initial commit

---
+++ Assets/GameController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField] private PlayerController player;
+    [SerializeField] private LevelController level;
+    [SerializeField] private Text scoreObject;
+    [SerializeField] private float difficulty;
+
+    private int score;
+
+    void Start()
+    {
+        Reset();
+    }
+
+    void Reset()
+    {
+        player.Reset();
+        level.Reset();
+
+        difficulty = 0;
+        SetScore(0);
+    }
+
+    private void SetScore(int newScore)
+    {
+        score = newScore;
+
+        scoreObject.text = score.ToString();
+    }
+
+    public void PlayerSucceed() {
+        SetScore(score += 100);
+    }
+
+    public void PlayerFailed() {
+        Reset();
+    }
+}
---
+++ Assets/LevelController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour
+{
+    [SerializeField] private SpawnerController spawner;
+    [SerializeField] private GameObject car;
+
+    private Vector2 lastSpawnerCoordinantes;
+
+    void Update()
+    {
+        
+    }
+
+    void Reset()
+    {
+        throw new NotImplementedException();
+    }
+
+    void GenerateSpawner(float difficulty) {
+        Instantiate(spawner, );
+    }
+}
